e88a90e3ceccef5297153b274784587ba0037bd1
DineshKokkiligadda <118521990+DineshKokkiligadda@users.noreply.github.com>
2024-07-25 15:42:08 +0530

    Add files via upload

---
+++ SpendSmartDB.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Web.Models
+{
+    public class SpendSmartDB : DbContext
+    {
+        public DbSet<Expence> Expence { get; set; }
+        public SpendSmartDB(DbContextOptions<SpendSmartDB> options) : base(options) { }
+    }
+}
